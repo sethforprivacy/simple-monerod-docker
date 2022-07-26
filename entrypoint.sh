39ace9856577c101d5737d4a77488d5cb13bbad5
Sam Al-Sapti <sam@sapti.me>
2022-07-26 18:56:40 +0200

    Make entrypoint.sh POSIX compliant

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,15 @@
-#!/bin/ash
+#!/bin/sh
 # Credit for the bulk of this entrypoint script goes to cornfeedhobo
 # Source is https://github.com/cornfeedhobo/docker-monero/blob/master/entrypoint.sh
 set -e
 
 # Set require --non-interactive flag
-set -- monerod --non-interactive "$@"
+set -- "monerod" "--non-interactive" "$@"
 
 # Configure NUMA if present for improved performance
-numa='numactl --interleave=all'
-if $numa true &> /dev/null; then
-	set -- $numa "$@"
+if command -v numactl >/dev/null 2>&1; then
+    numa="numactl --interleave=all"
+    set -- "$numa" "$@"
 fi
 # Start the daemon using fixuid
 # to adjust permissions if needed
